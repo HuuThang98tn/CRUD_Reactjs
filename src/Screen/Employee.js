f7c66cedfa8d8fbd241c6e42a436da274138e7e0
Tran Huu Thang <tranhuuthang98fpttn@gmail.com>
2023-01-11 11:42:00 +0700

    make it better

--- src/Screen/Employee.js
+++ src/Screen/Employee.js
@@ -161,7 +161,7 @@ const Employee = () => {
         axios(config)
             .then(function (response) {
                 console.log(JSON.stringify(response.data));
-                // window.location.reload()
+                window.location.reload()
             })
             .catch(function (error) {
                 console.log(error);
